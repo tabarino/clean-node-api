0c83fe03848eefda7920f4907f2e7619b0be352b
Ivan Tabarino <tabarino@outlook.com>
2021-08-28 11:28:38 +0100

    feat: ensure SignUpController returns 500 if EmailValidator throws an error

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,30 +1,38 @@
 import { Controller } from '../protocols/controller';
 import { HttpRequest, HttpResponse } from '../protocols/http';
-import { MissingParamError } from '../errors/missing-param-error';
-import { badRequest } from '../helpers/http-helper';
 import { EmailValidator } from '../protocols/email-validator';
+import { badRequest } from '../helpers/http-helper';
+import { MissingParamError } from '../errors/missing-param-error';
 import { InvalidParamError } from '../errors/invalid-param-error';
+import { ServerError } from '../errors/server-error';
 
 export class SignUpController implements Controller {
   constructor (private readonly _emailValidator: EmailValidator) { }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredField = ['name', 'email', 'password', 'passwordConfirmation'];
+    try {
+      const requiredField = ['name', 'email', 'password', 'passwordConfirmation'];
 
-    for (const field of requiredField) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+      for (const field of requiredField) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
-    }
 
-    const isValid = this._emailValidator.isValid(httpRequest.body.email);
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'));
-    }
+      const isValid = this._emailValidator.isValid(httpRequest.body.email);
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'));
+      }
 
-    return {
-      statusCode: 200,
-      body: ''
-    };
+      return {
+        statusCode: 200,
+        body: ''
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      };
+    }
   }
 }
