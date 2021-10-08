311b8ae316cb917066a751131e62e8c6c26601aa
Ivan Tabarino <tabarino@outlook.com>
2021-10-08 18:22:09 +0100

    refactor: move compare password validation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,3 +1,4 @@
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation';
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation';
 import { Validation } from '../../presentation/helpers/validators/validation';
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite';
@@ -8,10 +9,13 @@ jest.mock('../../presentation/helpers/validators/validation-composite');
 describe('Signup Validation Factory', () => {
   test('Should call ValidationComposite with all validations', () => {
     makeSignUpValidation();
+
     const validations: Validation[] = [];
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
       validations.push(new RequiredFieldValidation(field));
     }
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'));
+
     expect(ValidationComposite).toHaveBeenCalledWith(validations);
   });
 });
