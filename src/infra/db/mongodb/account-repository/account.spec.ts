9e67344a26b958fbf8304c00092aa5b37bb46c83
Ivan Tabarino <tabarino@outlook.com>
2021-10-16 12:53:16 +0100

    test: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -56,4 +56,17 @@ describe('Account Mongo Repository', () => {
     const account = await sut.loadByEmail('any_email@mail.com');
     expect(account).toBeFalsy();
   });
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut();
+    const result = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    });
+    await sut.updateAccessToken(result.insertedId.toString(), 'any_token');
+    const fakeaccount = await accountCollection.findOne({ _id: result.insertedId });
+    expect(fakeaccount).toBeTruthy();
+    expect(fakeaccount.accessToken).toBe('any_token');
+  });
 });
