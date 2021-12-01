b533a2ab1ce352558d7ec451d32bb28ef34d297b
Ivan Tabarino <tabarino@outlook.com>
2021-12-01 20:11:58 +0000

    test: ensure add survey route returns 204 if a valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,12 @@
 import request from 'supertest';
 import app from '../config/app';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
-import { Collection } from 'mongodb';
+import { Collection, ObjectId } from 'mongodb';
+import { sign } from 'jsonwebtoken';
+import env from '../config/env';
 
 let surveyCollection: Collection;
+let accountCollection: Collection;
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -17,6 +20,8 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys');
     await surveyCollection.deleteMany({});
+    accountCollection = await MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
   });
 
   describe('POST /surveys', () => {
@@ -31,6 +36,37 @@ describe('Survey Routes', () => {
           ]
         })
         .expect(403);
+    });
+
+    test('Should return 204 on add survey with valid access token', async () => {
+      const result = await accountCollection.insertOne({
+        name: 'Ivan',
+        email: 'tabarino@outlook.com',
+        password: '123',
+        role: 'admin'
+      });
+
+      const accountId = result.insertedId.toString();
+      const accessToken = sign({ accountId }, env.jwtSecret);
+      await accountCollection.updateOne({
+        _id: new ObjectId(accountId)
+      }, {
+        $set: {
+          accessToken
+        }
+      });
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'any_question',
+          answers: [
+            { image: 'http://image-name1.com', answer: 'any_answer' },
+            { image: 'http://image-name2.com', answer: 'any_answer2' }
+          ]
+        })
+        .expect(204);
     });
   });
 });
