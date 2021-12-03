1aa837dab7d829830695459bf5b4c23d8caf826a
Ivan Tabarino <tabarino@outlook.com>
2021-12-03 19:33:45 +0000

    refactor: add date on AddSurvey

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -31,7 +31,8 @@ describe('Survey Mongo Repository', () => {
         answer: 'any_answer'
       }, {
         answer: 'any_answer2'
-      }]
+      }],
+      date: new Date()
     });
     const survey = await surveyCollection.findOne({ question: 'any_question' });
     expect(survey).toBeTruthy();
