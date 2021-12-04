fec96d265d180f48ed41be18b67804ada681f7ad
Ivan Tabarino <tabarino@outlook.com>
2021-12-04 19:40:11 +0000

    test: ensure load surveys route returns 200 if a valid token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -77,35 +77,36 @@ describe('Survey Routes', () => {
         .expect(403);
     });
 
-    // test('Should return 204 on add survey with valid access token', async () => {
-    //   const result = await accountCollection.insertOne({
-    //     name: 'Ivan',
-    //     email: 'tabarino@outlook.com',
-    //     password: '123',
-    //     role: 'admin'
-    //   });
+    test('Should return 200 on load surveys with valid access token', async () => {
+      const result = await accountCollection.insertOne({
+        name: 'Ivan',
+        email: 'tabarino@outlook.com',
+        password: '123'
+      });
 
-    //   const accountId = result.insertedId.toString();
-    //   const accessToken = sign({ accountId }, env.jwtSecret);
-    //   await accountCollection.updateOne({
-    //     _id: new ObjectId(accountId)
-    //   }, {
-    //     $set: {
-    //       accessToken
-    //     }
-    //   });
+      const accountId = result.insertedId.toString();
+      const accessToken = sign({ accountId }, env.jwtSecret);
+      await accountCollection.updateOne({
+        _id: new ObjectId(accountId)
+      }, {
+        $set: {
+          accessToken
+        }
+      });
 
-    //   await request(app)
-    //     .post('/api/surveys')
-    //     .set('x-access-token', accessToken)
-    //     .send({
-    //       question: 'any_question',
-    //       answers: [
-    //         { image: 'http://image-name1.com', answer: 'any_answer' },
-    //         { image: 'http://image-name2.com', answer: 'any_answer2' }
-    //       ]
-    //     })
-    //     .expect(204);
-    // });
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      }]);
+
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .expect(200);
+    });
   });
 });
