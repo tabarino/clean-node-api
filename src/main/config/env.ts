fb6fa8757050068dac52086540a21c8a380ccb50
Ivan Tabarino <tabarino@outlook.com>
2022-04-30 14:32:24 +0100

    chore: adjust configs to be able to debug on local and docker

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
+  nodeEnv: process.env.NODE_ENV ?? 'development',
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT ?? 3000,
   jwtSecret: process.env.JWT_SECRET ?? 'tj650==5H123'
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,13 @@
-import 'module-alias/register';
-import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper';
+import moduleAlias from 'module-alias';
+import path from 'path';
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper';
 import env from './config/env';
+
+if (env.nodeEnv === 'development') {
+  moduleAlias.addAlias('@', path.join(__dirname, '..', '..', 'src'));
+} else {
+  moduleAlias.addAlias('@', path.join(__dirname, '..', '..', 'dist'));
+}
 
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
