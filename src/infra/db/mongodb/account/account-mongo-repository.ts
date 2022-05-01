580a43a33d594d09834aac36841996a9a3047c9c
Ivan Tabarino <tabarino@outlook.com>
2022-05-01 20:35:52 +0100

    refactor: organise project files

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -3,7 +3,7 @@ import { AddAccountRepository } from '@/data/protocols/db/account/add-account-re
 import { LoadAccountbyEmailRepository } from '@/data/protocols/db/account/load-account-by-email-repository';
 import { UpdateAccessTokenRepository } from '@/data/protocols/db/account/update-access-token-repository';
 import { AccountModel } from '@/domain/models/account';
-import { AddAccountModel } from '@/domain/usecases/add-account';
+import { AddAccountModel } from '@/domain/usecases/account/add-account';
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper';
 import { LoadAccountByTokenRepository } from '@/data/protocols/db/account/load-account-by-token-repository';
 
--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,10 @@
 import { ObjectId } from 'mongodb';
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper';
-import { AddSurveyModel } from '@/domain/usecases/add-survey';
-import { AddSurveyRepository } from '@/data/usecases/add-survey/db-add-survey-protocols';
+import { AddSurveyModel } from '@/domain/usecases/survey/add-survey';
+import { AddSurveyRepository } from '@/data/usecases/survey/add-survey/db-add-survey-protocols';
 import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-surveys-repository';
 import { SurveyModel } from '@/domain/models/survey';
-import { LoadSurveyByIdRepository } from '@/data/usecases/load-survey-by-id/db-load-survey-by-id-protocols';
+import { LoadSurveyByIdRepository } from '@/data/usecases/survey/load-survey-by-id/db-load-survey-by-id-protocols';
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
   async loadAll (): Promise<SurveyModel[]> {
