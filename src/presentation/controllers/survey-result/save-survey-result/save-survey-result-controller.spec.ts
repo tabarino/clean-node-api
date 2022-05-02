8545b2880f2130c8e9b8b16864f6fe0e996fb830
Ivan Tabarino <tabarino@outlook.com>
2022-05-02 11:55:14 +0100

    test: ensure SaveSurveyResultController calls Validation with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -9,7 +9,7 @@ import {
   LoadSurveyById
 } from './save-survey-result-controller-protocols';
 import { SaveSurveyResultController } from './save-survey-result-controller';
-import { ok, forbidden, serverError } from '@/presentation/helpers/http/http-helper';
+import { ok, forbidden, serverError, badRequest } from '@/presentation/helpers/http/http-helper';
 import { InvalidParamError } from '@/presentation/errors';
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -88,6 +88,21 @@ describe('SaveSurveyResult Controller', () => {
 
   afterAll(() => {
     MockDate.reset();
+  });
+
+  test('Should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut();
+    const validateSpy = jest.spyOn(validationStub, 'validate');
+    const httpRequest = makeFakeRequest();
+    await sut.handle(httpRequest);
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  test('Should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut();
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error());
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(badRequest(new Error()));
   });
 
   test('Should call LoadSurveyById with correct values', async () => {
