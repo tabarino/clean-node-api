6cda76462ed48f8fb833cf6e337a6feeb91e5fb3
Ivan Tabarino <tabarino@outlook.com>
2022-05-04 12:14:32 +0100

    feat: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct values

--- src/data/test-helpers/mock-survey-result.ts
+++ src/data/test-helpers/mock-survey-result.ts
@@ -1,7 +1,17 @@
 import { SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result';
 import { SurveyResultModel } from '@/domain/models/survey-result';
 import { mockSurveyResultModel } from '@/domain/test-helpers/mock-survey-result';
+import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository';
 import { SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/save-survey-result-repository';
+
+export const mockLoadSurveyResultRepository = (): LoadSurveyResultRepository => {
+  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+    async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+      return await Promise.resolve(mockSurveyResultModel());
+    }
+  }
+  return new LoadSurveyResultRepositoryStub();
+};
 
 export const mockSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
   class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
