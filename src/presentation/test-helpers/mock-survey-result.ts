9ad0cb78f254382415855a8bf8f035f784da279a
Ivan Tabarino <tabarino@outlook.com>
2022-05-04 19:30:43 +0100

    feat: ensure LoadSurveyResultController calls LoadSurveyById and LoadSurveyResult with correct values

--- src/presentation/test-helpers/mock-survey-result.ts
+++ src/presentation/test-helpers/mock-survey-result.ts
@@ -1,6 +1,16 @@
 import { SurveyResultModel } from '@/domain/models/survey-result';
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result';
 import { SaveSurveyResult, SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result';
 import { mockSurveyResultModel } from '@/domain/test-helpers';
+
+export const mockLoadSurveyResult = (): LoadSurveyResult => {
+  class LoadSurveyResultStub implements LoadSurveyResult {
+    async load (surveyId: string): Promise<SurveyResultModel> {
+      return Promise.resolve(mockSurveyResultModel());
+    }
+  }
+  return new LoadSurveyResultStub();
+};
 
 export const mockSaveSurveyResult = (): SaveSurveyResult => {
   class SaveSurveyResultStub implements SaveSurveyResult {
