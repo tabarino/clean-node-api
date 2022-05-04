f542c36a54e71b233b52cab468cc4fe80559b4f1
Ivan Tabarino <tabarino@outlook.com>
2022-05-04 14:56:04 +0100

    feat: ensure SurveyResultMongoRepository loads surveyResults correct

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -1,12 +1,11 @@
 import { ObjectId } from 'mongodb';
 import { MongoHelper, QueryBuilder } from '@/infra/db/mongodb/helpers';
-import {
-  SaveSurveyResultParams,
-  SaveSurveyResultRepository,
-  SurveyResultModel
-} from '@/data/usecases/survey-result/save-survey-result/db-save-survey-result-protocols';
+import { SurveyResultModel } from '@/domain/models/survey-result';
+import { SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result';
+import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository';
+import { SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/save-survey-result-repository';
 
-export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+export class SurveyResultMongoRepository implements LoadSurveyResultRepository, SaveSurveyResultRepository {
   async save (data: SaveSurveyResultParams): Promise<SurveyResultModel> {
     const surveyResultCollection = await MongoHelper.getCollection('surveyResults');
     await surveyResultCollection.findOneAndUpdate({
@@ -24,7 +23,7 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
     return await this.loadBySurveyId(data.surveyId)
   }
 
-  private async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+  async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
     const surveyResultCollection = await MongoHelper.getCollection('surveyResults');
     const query = new QueryBuilder()
       .match({
