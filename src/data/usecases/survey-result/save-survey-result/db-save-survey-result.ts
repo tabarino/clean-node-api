1d6afa1b307974856705fc006aec5f60fb716a0d
Ivan Tabarino <tabarino@outlook.com>
2022-05-04 18:47:21 +0100

    feat: ensure DbLoadSurveyResult and DbSaveSurveyResult return empty if there is no result

--- src/data/usecases/survey-result/save-survey-result/db-save-survey-result.ts
+++ src/data/usecases/survey-result/save-survey-result/db-save-survey-result.ts
@@ -3,18 +3,30 @@ import {
   SaveSurveyResult,
   SaveSurveyResultParams,
   SaveSurveyResultRepository,
-  LoadSurveyResultRepository
+  LoadSurveyResultRepository,
+  LoadSurveyByIdRepository
 } from './db-save-survey-result-protocols';
 
 export class DbSaveSurveyResult implements SaveSurveyResult {
   constructor (
+    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository,
     private readonly loadSurveyResultRepository: LoadSurveyResultRepository,
     private readonly saveSurveyResultRepository: SaveSurveyResultRepository
   ) { }
 
   async save (data: SaveSurveyResultParams): Promise<SurveyResultModel> {
     await this.saveSurveyResultRepository.save(data);
-    const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(data.surveyId);
+    let surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(data.surveyId);
+    if (!surveyResult) {
+      const survey = await this.loadSurveyByIdRepository.loadById(data.surveyId);
+      surveyResult = {
+        surveyId: survey.id,
+        question: survey.question,
+        date: survey.date,
+        answers: survey.answers.map(answer => ({ ...answer, count: 0, percent: 0 }))
+      }
+    }
+
     return surveyResult;
   }
 }
