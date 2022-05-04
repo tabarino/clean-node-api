60d13c29f5d3063fd74f4aa3303ad9a64bd913d3
Ivan Tabarino <tabarino@outlook.com>
2022-05-04 23:17:13 +0100

    feat: create LoadSurveyResult composition

--- src/main/routes/survey-result-routes.ts
+++ src/main/routes/survey-result-routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { adaptRoute } from '@/main/adapters/express/express-router-adapter';
 import { makeSaveSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory';
+import { makeLoadSurveyResultController } from '@/main/factories/controllers/survey-result/load-survey-result/load-survey-result-controller-factory';
 import { auth } from '@/main/middlewares/auth';
 
 export default (router: Router): void => {
   router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()));
+  router.get('/surveys/:surveyId/results', auth, adaptRoute(makeLoadSurveyResultController()));
 };
